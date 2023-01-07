be12d2b532185052befb1cf501fb6adb5fd6bc14
Ostapenko Ivan <ostapenko.dmytro05@gmail.com>
2023-01-07 17:45:31 +0200

    Fixed issue with TypeORM config and started to work on Auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,18 +2,23 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signup(authCredentialsDto);
   }
 
-  async signin(authCredentialsDto: AuthCredentialsDto) {
+  async signin(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const username = await this.userRepository.validateUserPassword(
       authCredentialsDto,
     );
@@ -21,5 +26,10 @@ export class AuthService {
     if (!username) {
       throw new UnauthorizedException('Invalid credentials');
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
--- src/tasks/config/typeorm.config.ts
+++ src/tasks/config/typeorm.config.ts
@@ -7,7 +7,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'postgres',
   password: '124578',
   database: 'taskmanagement',
-  entities: ['dist/tasks/*.entity.{ts,js}'],
+  entities: ['dist/**/*.entity.{ts,js}'],
   synchronize: true,
   autoLoadEntities: true,
 };
