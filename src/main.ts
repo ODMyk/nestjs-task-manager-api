377ca19a08fd0c201f383f84f02dcf9e6c9cc054
Ostapenko Ivan <ostapenko.dmytro05@gmail.com>
2023-01-11 18:48:37 +0200

    Installed config package and applied configuration for database and server

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
-  const PORT = 3000;
+  const PORT = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   await app.listen(PORT);
   logger.log(`Application started on port ${PORT}`);
 }
