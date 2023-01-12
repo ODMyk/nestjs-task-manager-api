0ae00ed88f40bbc3004a9cbe00b1bd173cac5629
Ostapenko Ivan <ostapenko.dmytro05@gmail.com>
2023-01-12 16:28:21 +0200

    Created first basic swagger page

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as config from 'config';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Task manager API')
+    .setDescription('What I should type in description ?')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
   const logger = new Logger('bootstrap');
   const PORT = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
